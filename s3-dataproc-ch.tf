a6301cf38a357b39f4d3fb06d85e9a3f977c24dc
Mikhail Darkhovskii <darkhovskii@yandex-team.ru>
2026-05-04 21:42:08 +0500

    Update to resource v2 for clickhouse cluster

--- s3-dataproc-ch.tf
+++ s3-dataproc-ch.tf
@@ -247,40 +247,47 @@ resource "yandex_dataproc_cluster" "dataproc-cluster" {
   }
 }
 
-resource "yandex_mdb_clickhouse_cluster" "mch-cluster" {
+resource "yandex_mdb_clickhouse_cluster_v2" "mch-cluster" {
   description        = "Managed Service for ClickHouse cluster"
   name               = "mch-cluster"
   environment        = "PRODUCTION"
   network_id         = yandex_vpc_network.dataproc_ch_network.id
   security_group_ids = [yandex_vpc_security_group.mch_security_group.id]
 
-  clickhouse {
-    resources {
+  clickhouse = {
+    resources = {
       resource_preset_id = "s2.micro" # 2 vCPU, 8 GB RAM
       disk_type_id       = "network-ssd"
       disk_size          = 10 # GB
     }
   }
 
-  host {
-    type             = "CLICKHOUSE"
-    zone             = "ru-central1-a"
-    subnet_id        = yandex_vpc_subnet.dataproc_ch_subnet-a.id
-    assign_public_ip = true # Required for connection from the Internet
+  hosts = {
+    "ch-host1" = {
+      type             = "CLICKHOUSE"
+      zone             = "ru-central1-a"
+      subnet_id        = yandex_vpc_subnet.dataproc_ch_subnet-a.id
+      assign_public_ip = true # Required for connection from the Internet
+      shard_name       = "shard1"
+    }
   }
 
-  lifecycle {
-    ignore_changes = [database, user,]
+  shards = {
+    "shard1" = {}
+  }
+
+  maintenance_window {
+    type = "ANYTIME"
   }
 }
 
 resource "yandex_mdb_clickhouse_database" "db1" {
-  cluster_id = yandex_mdb_clickhouse_cluster.mch-cluster.id
+  cluster_id = yandex_mdb_clickhouse_cluster_v2.mch-cluster.id
   name       = "db1"
 }
 
 resource "yandex_mdb_clickhouse_user" "user1" {
-  cluster_id = yandex_mdb_clickhouse_cluster.mch-cluster.id
+  cluster_id = yandex_mdb_clickhouse_cluster_v2.mch-cluster.id
   name       = "user1"
   password   = local.ch_password
 
